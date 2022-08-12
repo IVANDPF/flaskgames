a189d3dcd20836acb52bc38701e7672b858b0f33
Ivan <ivanmdelpozof@gmail.com>
2022-08-11 20:44:25 -0500

    entire project

---
+++ esquema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE juegos (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre_ VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    precio int NOT NULL
+    PRIMARY KEY (id)
+)
+
